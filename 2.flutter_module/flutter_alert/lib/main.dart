d608d3c6de433835e7bb91a70d3494205a557f93
zhang.yili <927311726@qq.com>
2019-08-22 12:52:50 +0800

    添加打包流程

--- 2.flutter_module/flutter_alert/lib/main.dart
+++ 2.flutter_module/flutter_alert/lib/main.dart
@@ -1,110 +1,139 @@
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+//void main() => runApp(MyApp());
+void main() => runApp(_widgetForRouter(ui.window.defaultRouteName));
+
+//根据iOS端传过来的router跳转不同的页面
+Widget _widgetForRouter(String route) {
+  switch(route) {
+    case 'myApp':
+    return new MyApp();
+    case 'home':
+    return new HomePageWidget(title: 'test456',);
+    default:
+    return Center(
+      child: Text('Unknown route: $route', textDirection: TextDirection.ltr),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Bridge Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in a Flutter IDE). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePageWidget(title: 'test123',),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class HomePageWidget extends StatefulWidget {
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  HomePageWidget({Key key, this.title}) :super(key: key);
   final String title;
 
-  @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<HomePageWidget> {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  //创建一个给native的chanel
+  //static const methodChannel = const MethodChannel('com.pages.your/native_get');
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+
+    //调用弹出框
+    void _showCustomAlert() async {
+      //发送消息给native
+      //Map<String, String> map = {"title": "用户点击了中心按钮"};
+      //methodChannel.invokeMapMethod('toNativePush',map);
+
+      return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('警告弹出框'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text('line1'),
+              Text('line2'),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        actions: <Widget>[
+          FlatButton(
+            child: Text('知道了!'),
+            onPressed: () {
+              Navigator.of(context).pop();
+              //Map<String, String> map = {"title": "弹出框确认点击"};
+              //methodChannel.invokeMapMethod('dismissAlert',map);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        // const RaisedButton(
+        //   onPressed: null,
+        //   child: Text(
+        //     '调用弹出框',
+        //     style: TextStyle(fontSize: 20)
+        //   ),
+        // ),
+        const SizedBox(height: 30),
+        RaisedButton(
+          onPressed: _showCustomAlert,
+          child: const Text(
+            'Enabled Button1',
+            style: TextStyle(fontSize: 20)
+          ),
+        ),
+        const SizedBox(height: 30),
+        // RaisedButton(
+        //   onPressed: () {},
+        //   textColor: Colors.white,
+        //   padding: const EdgeInsets.all(0.0),
+        //   child: Container(
+        //     decoration: const BoxDecoration(
+        //       gradient: LinearGradient(
+        //         colors: <Color>[
+        //           Color(0xFF0D47A1),
+        //           Color(0xFF1976D2),
+        //           Color(0xFF42A5F5),
+        //         ],
+        //       ),
+        //     ),
+        //     padding: const EdgeInsets.all(10.0),
+        //     child: const Text(
+        //       'Gradient Button2',
+        //       style: TextStyle(fontSize: 20)
+        //     ),
+        //   ),
+        // ),
+      ],
+    ),    
+  ),
+);
+  
+
+
+
   }
 }
+
